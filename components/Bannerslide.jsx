0c3929712d151eb441fd5266190a1d3a7698e76d
aedevs2026-oss <aedevs2026@gmail.com>
2026-07-23 09:32:18 +0530

    Update Bannerslide.jsx
    2

--- components/Bannerslide.jsx
+++ components/Bannerslide.jsx
@@ -5,11 +5,9 @@ import { useEffect, useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const banners = [
+  "/aedevs-4.png",,
   "/aedevs-1.png",
   "/aedevs-5.png",
-  "/aedevs-2.png",
-  "/aedevs-3.png",
-  "/aedevs-4.png",
 ];
 
 export default function BannerSlider() {
@@ -32,16 +30,15 @@ export default function BannerSlider() {
   }, [current]);
 
   return (
-    <div className="relative aspect-[16/9] lg:h-[700px] w-full overflow-hidden rounded-2xl lg:rounded-[30px] shadow-2xl">
+    <div className="relative h-[240px] sm:h-[320px] md:h-[450px] lg:h-[700px] w-full overflow-hidden">
       {/* Images */}
       {banners.map((banner, index) => (
         <Image
-          key={banner}
           src={banner}
           alt={`Banner ${index + 1}`}
           fill
           priority={index === 0}
-          className={`absolute inset-0 object-cover transition-opacity duration-1000 ${
+          className={`absolute inset-0 object-contain lg:object-cover transition-opacity duration-1000 ${
             current === index ? "opacity-100" : "opacity-0"
           }`}
         />
@@ -50,7 +47,7 @@ export default function BannerSlider() {
       {/* Left Button */}
       <button
         onClick={prevSlide}
-        className="hidden md:flex absolute left-5 top-1/2 -translate-y-1/2 z-20 items-center justify-center rounded-full bg-gray-100/90 p-3 shadow-lg transition hover:bg-white hover:scale-110"
+        className="relative aspect-[16/9] lg:h-[700px] w-full overflow-hidden rounded-2xl lg:rounded-[30px] shadow-2xl"
       >
         <ChevronLeft
           size={20}
@@ -61,7 +58,7 @@ export default function BannerSlider() {
       {/* Right Button */}
       <button
         onClick={nextSlide}
-        className="hidden md:flex absolute right-5 top-1/2 -translate-y-1/2 z-20 items-center justify-center rounded-full bg-gray-100/90 p-3 shadow-lg transition hover:bg-white hover:scale-110"
+        className="absolute right-2 sm:right-4 lg:right-5 top-1/2 -translate-y-1/2 z-20 rounded-full bg-gray-100/90 p-2 sm:p-3 shadow-lg transition hover:bg-white hover:scale-110"
       >
         <ChevronRight
           size={20}
